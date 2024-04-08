d8ca26bed6205e6b43df6ace56c3eed07f7816c3
Jaaron <jsw6194@rit.edu>
2024-04-07 22:36:15 -0400

    deleting works

--- client/maker.jsx
+++ client/maker.jsx
@@ -9,13 +9,14 @@ const handleDomo = (e, onDomoAdded) => {
 
     const name = document.querySelector('#domoName').value;
     const age = document.querySelector('#domoAge').value;
+    const color = document.querySelector('#domoColor').value;
 
-    if (!name || !age) {
+    if (!name || !age || !color) {
         helper.handleError('All fields are required');
         return false;
     }
 
-    helper.sendPost(e.target.action, { name, age }, onDomoAdded);
+    helper.sendPost(e.target.action, { name, age, color }, onDomoAdded);
     return false;
 };
 
@@ -26,6 +27,8 @@ const DomoForm = (props) => {
             <input id="domoName" type="text" name="name" placeholder="Domo Name" />
             <label htmlFor="age">Age: </label>
             <input id="domoAge" type="number" min="0" name="age" />
+            <label htmlFor="color">Color: </label>
+            <input id="domoColor" type="text" name="color" placeholder="Domo Color" />
             <input className="makeDomoSubmit" type="submit" value="Make Domo" />
 
         </form>
@@ -53,11 +56,19 @@ const DomoList = (props) => {
     }
 
     const domoNodes = domos.map((domo) => {
+        console.log(domo);
         return (
             <div key={domo._id} className="domo">
                 <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
                 <h3 className="domoName">Name: {domo.name}</h3>
                 <h3 className="domoAge">Age: {domo.age}</h3>
+                <h3 className="domoColor">Color: {domo.color}</h3>
+                <a href="#" className="deleteDomo" onClick={(e)=>{
+                    e.target.parentNode.querySelector('.domoName').value;
+                    e.target.parentNode.querySelector('.domoAge').value;
+                    e.target.parentNode.querySelector('.domoColor').value;
+                    helper.sendPost('/deleteDomo', {name: domo.name, age: domo.age, color: domo.color}, props.triggerReload);
+                }}>Delete</a>
             </div>
         );
     });
@@ -78,7 +89,7 @@ const App = () => {
                 <DomoForm triggerReload={() => { setReloadDomos(!reloadDomos) }} />
             </div>
             <div id="domos">
-                <DomoList domos={[]} reloadDomos={reloadDomos} />
+                <DomoList domos={[]} reloadDomos={reloadDomos} triggerReload={() => { setReloadDomos(!reloadDomos) }}/>
             </div>
 
         </div>
--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -7,7 +7,7 @@ const makerPage = (req, res) => res.render('app');
 const getDomos = async (req, res) => {
   try {
     const query = { owner: req.session.account._id };
-    const docs = await Domo.find(query).select('name age').lean().exec();
+    const docs = await Domo.find(query).select('name age color').lean().exec();
 
     return res.json({ domos: docs });
   } catch (err) {
@@ -16,21 +16,33 @@ const getDomos = async (req, res) => {
   }
 };
 
+const deleteDomo = async (req, res) => {
+  try{
+    const id = {name: req.body.name, age: req.body.age, color: req.body.color};
+    const domo = await Domo.findOneAndDelete(id).exec();
+    return res.status(200).json({message: 'Domo deleted!'});
+  }catch(err){
+    console.log(err);
+    return res.status(500).json({error: 'Error deleting domo!'});
+  }
+};
+
 const makeDomo = async (req, res) => {
-  if (!req.body.name || !req.body.age) {
-    return res.status(400).json({ error: 'Both name and age are required' });
+  if (!req.body.name || !req.body.age || !req.body.color) {
+    return res.status(400).json({ error: 'All fields are required' });
   }
 
   const domoData = {
     name: req.body.name,
     age: req.body.age,
     owner: req.session.account._id,
+    color: req.body.color,
   };
 
   try {
     const newDomo = new Domo(domoData);
     await newDomo.save();
-    return res.status(201).json({ name: newDomo.name, age: newDomo.age });
+    return res.status(201).json({ name: newDomo.name, age: newDomo.age, color: newDomo.color});
   } catch (err) {
     console.log(err);
     if (err.code === 11000) {
@@ -44,4 +56,5 @@ module.exports = {
   makerPage,
   makeDomo,
   getDomos,
+  deleteDomo,
 };
